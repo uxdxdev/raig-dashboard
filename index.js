8b39814f0afbf1fec09754dc5478c4e2e8525fc9
David Morton <davidmortonmail@gmail.com>
2015-12-16 20:02:01 +0000

    Added secret message

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/becca', function(request, response) {
+  response.send('Becca is the best x');
+  response.end();
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
