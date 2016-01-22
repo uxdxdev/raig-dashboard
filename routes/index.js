793843044e7466d2ba249f5bcba7201aa037705a
David Morton <davidmortonmail@gmail.com>
2016-01-22 20:41:12 +0000

    Added GET api endpoint

--- routes/index.js
+++ routes/index.js
@@ -48,4 +48,37 @@ router.post('/api/v1/raig', function(request, response) {
     });
 });
 
+// Read
+router.get('/api/v1/raig', function(request, response) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return response.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return response.json(results);
+        });
+
+    });
+
+});
+
+
 module.exports = router;
